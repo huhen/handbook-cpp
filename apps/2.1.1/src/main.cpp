73be352603f6fa451e5bf1e2fba5b803855d9bc1
huhen <kinetic@bk.ru>
2025-04-22 17:00:27 +0300

    Start

---
+++ apps/2.1.1/src/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main(int, char **)
+{
+    std::cout << "C++ is a general-purpose programming language with a bias towards systems programming that\n"
+                 "  - is a better C\n"
+                 "  - supports data abstraction\n"
+                 "  - supports object-oriented programming\n"
+                 "  - supports generic programming.\n";
+}
